72473b94718050aedb4da0aaef5c2ffd49ba7c27
mohwildan <mobafreewtf@gmail.com>
2022-10-30 02:24:00 +0700

    refactor(add title): title Simple Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
   return (
     <html>
       <head>
-        <title>Wildan</title>
+        <title>Simple NavBar</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body>
